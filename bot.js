f5ce1f40805d8cbc4e8772e8504e9b542027a185
hopa332211 <49374617+hopa332211@users.noreply.github.com>
2019-04-09 16:45:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,14 @@
-const os = require('os');
-client.on('message', message => {
+cconst Discord = require('discord.js');
+const client = new Discord.Client();
 
-  let totalDMs = client.channels.filter(function (s) {
-      if (s.type && s.type === 'dm') {
-          return true;
-      }
-      return false;
-  })
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-  let totalTextChannels = client.channels.filter(function (s) {
-      if (s.type && s.type === 'text') {
-          return true;
-      }
-      return false;
-  })
-
-  let cpu = os.cpus();
-  let cpuArray = cpu[0].model.replace(/\s+/g, " ").trim().split(" ");
-  let cpuCores = cpu.length;
-  l  if (message.content.startsWith(prefix + "stats") || message.content.startsWith(prefix + "bot")) {
-      message.channel.send({
-          embed: new Discord.RichEmbed()
-              .setColor('#36393e')
-              .setTitle(' ℹ Bot Information')
-              .addField('» Ping :', **\${client.ping}` ms.,true)
-              .addField('» RAM Usage :',`${(process.memoryUsage().rss / 1048576).toFixed()}` MB.,true)
-              .addField('» Guilds :',`${client.guilds.size}`,true)
-              .addField('» Channels :',`${client.channels.size}`,true)
-              .addField('» User :',`${client.users.size}`,true)
-              .addField('» Name :',`${client.user.tag})`,true)
-              .addField('» Platform:',`${os.platform()}`,true)
-              .addField('» CPU :',`${cpuArray[0]} ${cpuArray[1]}`,true)
-              .addField('» CPU Specs :',`${cpuCores} @ ${cpuSpeed}`,true)
-              .addField('» Private Conversations :',`${totalDMs.size}`,true)
-              .addField('» Host :',`FlixHost`,true)
-              .addField('» WebSite :',`Soon`,true)
-              .addField('» Developers :',@# " / لُوُرًاُنَسٍ  / " #**`)
-      })
+client.on('message', msg => {
+  if (msg.content === '-bot') {
+    msg.reply('سرعت البوت (ms24)!');
   }
-})et cpuSpeed = (cpu[0].speed / 1000) + "MHZ";
+});
+
 client.login(process.env.BOT_TOKEN);
