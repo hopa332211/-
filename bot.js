7750207a6fae4165a3b05ba03dbe69c9980ffdd7
hopa332211 <49374617+hopa332211@users.noreply.github.com>
2019-04-09 16:33:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,41 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const os = require('os');
+client.on('message', message => {
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  let totalDMs = client.channels.filter(function (s) {
+      if (s.type && s.type === 'dm') {
+          return true;
+      }
+      return false;
+  })
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  let totalTextChannels = client.channels.filter(function (s) {
+      if (s.type && s.type === 'text') {
+          return true;
+      }
+      return false;
+  })
+
+  let cpu = os.cpus();
+  let cpuArray = cpu[0].model.replace(/\s+/g, " ").trim().split(" ");
+  let cpuCores = cpu.length;
+  l  if (message.content.startsWith(prefix + "stats") || message.content.startsWith(prefix + "bot")) {
+      message.channel.send({
+          embed: new Discord.RichEmbed()
+              .setColor('#36393e')
+              .setTitle(' ℹ Bot Information')
+              .addField('» Ping :', **\${client.ping}` ms.,true)
+              .addField('» RAM Usage :',`${(process.memoryUsage().rss / 1048576).toFixed()}` MB.,true)
+              .addField('» Guilds :',`${client.guilds.size}`,true)
+              .addField('» Channels :',`${client.channels.size}`,true)
+              .addField('» User :',`${client.users.size}`,true)
+              .addField('» Name :',`${client.user.tag})`,true)
+              .addField('» Platform:',`${os.platform()}`,true)
+              .addField('» CPU :',`${cpuArray[0]} ${cpuArray[1]}`,true)
+              .addField('» CPU Specs :',`${cpuCores} @ ${cpuSpeed}`,true)
+              .addField('» Private Conversations :',`${totalDMs.size}`,true)
+              .addField('» Host :',`FlixHost`,true)
+              .addField('» WebSite :',`Soon`,true)
+              .addField('» Developers :',@# " / لُوُرًاُنَسٍ  / " #**`)
+      })
   }
-});
-
-client.login(process.env.BOT_TOKEN);
+})et cpuSpeed = (cpu[0].speed / 1000) + "MHZ";
